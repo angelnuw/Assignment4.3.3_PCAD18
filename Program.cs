003ce9fd450bcc69ae7044b11050b1f7656effe1
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-07-30 15:37:51 -0400

    added comments

--- Program.cs
+++ Program.cs
@@ -10,25 +10,25 @@
             Console.WriteLine("input 3 elements of the aray: ");
             
             //print unique elements in the array
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) // Loop to read elements into the array
             {
-                arr[i] = Convert.ToInt32(Console.ReadLine());
+                arr[i] = Convert.ToInt32(Console.ReadLine()); // Read each element from the console
             }
             Console.WriteLine("The unique elements in the array are: ");
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)// Loop to find and print unique elements
             {
-                bool isUnique = true;
-                for (int j = 0; j < n; j++)
+                bool isUnique = true; // Assume the current element is unique
+                for (int j = 0; j < n; j++) // Loop to compare the current element with all other elements
                 {
-                    if (i != j && arr[i] == arr[j])
+                    if (i != j && arr[i] == arr[j]) // If the current element is found in another position
                     {
-                        isUnique = false;
+                        isUnique = false; // Set isUnique to false
                         break;
                     }
                 }
-                if (isUnique)
+                if (isUnique) // If the current element is unique
                 {
-                    Console.Write(arr[i] + " ");
+                    Console.Write(arr[i] + " "); // Print the unique element
                 }
             }
             Console.WriteLine();
